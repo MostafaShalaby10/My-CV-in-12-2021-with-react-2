9f65fb1298b1181a1ab588266a986d082ed54be9
Mostafa Shalaby <ms2231710@gmail.com>
2021-12-08 14:11:52 +0200

    First commit

---
+++ src/Home.js
@@ -0,0 +1,62 @@
+import react from "react";
+import './Home.css'
+
+function Home() {
+    return(
+        <div>
+                 <div className="container">
+                    <div className="left">
+                    <div className="text">
+                    <table>
+                    <tr>
+                      
+                      <td><b> Student</b></td>
+                    </tr>
+                    <tr>
+                        <td> <b>Menofia Egypt </b></td>
+                    </tr>
+                    <tr>
+                        <td> <b>01092637057</b></td>
+                    </tr>
+                </table> 
+                <a href="https://www.facebook.com/mostafa.shalaby.526/" target="_blank">Fcebook</a>
+                <a href="https://www.instagram.com/_mostafa_shalaby/?fbclid=IwAR14LFSzXjDEX811sjs3uzh6biarNvUbCLELwBN09s1dkXIZv9lT81XPH7g" target="_blank">Instgram</a>
+                <a href="https://twitter.com/mostafa_4alaby?fbclid=IwAR2Gb1aeaOfTlINPkDDkwLavrJw2vYdh9q_zot7SzcJVX8xub7D0HlfhfaI" target="_blank">Twitter</a>
+                <a href="https://www.linkedin.com/in/mostafa-shalaby-b32b1b1b1/" target="_blank">Linkedin</a>
+                <a href="https://github.com/MostafaShalaby10" target="_blank">Github</a>
+                <a href="http://ms2231710@gmail.com" target="_blank">Contact</a>
+            
+                </div>
+            </div>
+            <div className="right">
+                <div className="top">
+                    <h3>Education</h3>
+                    <p>Student at faculty of computer and Information science Menofia Universty</p>
+                    <h3>certifications</h3>
+                    <a href="https://drive.google.com/file/d/1TRixHAZamc8sZSTyitLb-VCiHdNqLn3C/view?usp=sharing" target="_blank">Desktop Application</a>
+                    <a href="https://drive.google.com/file/d/1R5oV9__L4nXtJOn7_NId5b1c309MtQ19/view?usp=sharing" target="_blank">HTML&CSS</a>
+                    </div>
+                <div className="bottom">
+                    <h3>Skills</h3>
+                    <div className="skills">
+                        <li>C++</li>
+                        <li>C#</li>
+                        <li>Java</li>
+                        <li>OPP</li>
+                        <li>SQL</li>
+                        <li>Dart</li>
+                        <li>HTML</li>
+                        <li>CSS</li>
+                    </div> 
+                    <h3>Languages</h3>
+                    <p>The mother language is <b>Arabic</b></p>
+                    <p>The seconed language is <b>English</b></p>     
+                
+                </div>
+            </div>
+        </div>
+             
+        </div>
+    )
+}
+export default Home
